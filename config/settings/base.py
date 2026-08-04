4ebb14f34f73132364b2bd10ca50b99222ed397a
mokulraj <s.p.mokulraj189@gmail.com>
2026-08-05 00:16:20 +0530

    feat: configure production-ready base Django settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,12 +1,31 @@
 from pathlib import Path
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-SECRET_KEY = "django-insecure-change-this-later"
+env = environ.Env()
 
-DEBUG = True
+environ.Env.read_env(BASE_DIR / ".env")
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env.bool("DEBUG", default=False)
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+LANGUAGE_CODE = env(
+    "LANGUAGE_CODE",
+    default="en-us",
+)
+
+TIME_ZONE = env(
+    "TIME_ZONE",
+    default="Asia/Kolkata",
+)
+
+USE_I18N = True
+
+USE_TZ = True
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -45,16 +64,18 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
@@ -66,14 +87,46 @@ DATABASES = {
     }
 }
 
-LANGUAGE_CODE = "en-us"
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
-TIME_ZONE = "Asia/Kolkata"
+STATIC_URL = "/static/"
 
-USE_I18N = True
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-USE_TZ = True
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "logs/django.log",
+        },
+    },
+    "root": {
+        "handlers": ["file"],
+        "level": "INFO",
+    },
+}
